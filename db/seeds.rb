e5ce682b24e14dbfb1dbc54ea33cdf1d3ceb81ac
Daniel Chu <danchu123@gmail.com>
2016-12-29 15:00:06 -0600

    Product seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,17 +24,80 @@
 #     )
 # end
 
-20.times do |user_id|
-  10.times do |product_id|
-    Visit.create(
-        user_id: (user_id + 1),
-        product_id: (product_id + 1),
-        time: Time.now,
-        ip: "50.200.5.113",
-        country: "location.country",
-        city: "location.city",
-        state: "location.state",
-        postal_code: "location.postal_code"
-      )
-  end
-end
+# 20.times do |user_id|
+#   10.times do |product_id|
+#     Visit.create(
+#         user_id: (user_id + 1),
+#         product_id: (product_id + 1),
+#         time: Time.now,
+#         ip: "50.200.5.113",
+#         country: "location.country",
+#         city: "location.city",
+#         state: "location.state",
+#         postal_code: "location.postal_code"
+#       )
+#   end
+# end
+
+Product.create(
+    product_number: "123.456.1",
+    product_name: "Cuisinart Hot Air Popcorn Make",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/cuisinart-hot-air-popcorn-maker/s332553"
+  )
+
+Product.create(
+    product_number: "123.456.2",
+    product_name: "Scalloped Melamine Popcorn Tub",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/scalloped-melamine-popcorn-tub/s389640?st=Scalloped%20Melamine%20Popcorn%20Tub&ta=scalloped"
+  )
+
+Product.create(
+    product_number: "123.456.3",
+    product_name: "Chefn Pop Top Microwave Popcorn Popper",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/chefn-pop-top-microwave-popcorn-popper/s529564?st=Chef%27n%20%C2%AE%20Pop%20Top%20Microwave%20Popcorn%20Popper&ta=poptop"
+  )
+
+Product.create(
+    product_number: "123.456.4",
+    product_name: "Pilsner Beer Glass",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/direction-17-oz.-pilsner-beer-glass/s569143?st=Direction%2017%20oz.%20Pilsner%20Beer%20Glass&ta=direction%20pilsner%20beer"
+  )
+
+Product.create(
+    product_number: "123.456.5",
+    product_name: "Stovetop Popcorn Popper Black",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/stovetop-popcorn-popper-black/s497401"
+  )
+
+Product.create(
+    product_number: "123.456.6",
+    product_name: "Red Spatterware Spouted Bowls Set Of 2",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/red-spatterware-spouted-bowls-set-of-2/s133564?st=Red%20Spatterware%20Spouted%20Bowls%20Set%20of%202&ta=set%20of%202%20red"
+  )
+
+Product.create(
+    product_number: "123.456.7",
+    product_name: "Set Of 4 Iittala Krouvi Beer Mugs",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/set-of-4-iittala-krouvi-beer-mugs/s579044?st=Set%20of%204%20Iittala%20Krouvi%20Beer%20Mugs&ta=littala"
+  )
+
+Product.create(
+    product_number: "123.456.8",
+    product_name: "Can Glass",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/can-glass/s430391"
+  )
+
+Product.create(
+    product_number: "123.456.9",
+    product_name: "Zippy Pop Stovetop Popcorn Maker",
+    retailer: "Crate & Barrel",
+    url: "http://www.crateandbarrel.com/zippy-pop-stovetop-popcorn-maker/s643384?st=Zippy%20Pop%20Stovetop%20Popcorn%20Maker"
+  )
