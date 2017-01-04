d3fe81438465e2e6471aa72417344ced42f1d6d9
Daniel Chu <danchu123@gmail.com>
2017-01-04 10:44:22 -0600

    Common user cities

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -6,6 +6,7 @@ class VisitsController < ApplicationController
     @user_count = User.count
     @most_frequent_os = User.top(:device_os, 1).keys[0]
     @most_frequent_type = User.top(:device_type, 1).keys[0]
+    @common_user_city = Visit.top(:city, 1).keys[0]
     most_viewed_product = Visit.top(:product_id).keys[0]
     @most_viewed_product = Product.find(most_viewed_product).product_name
   end
