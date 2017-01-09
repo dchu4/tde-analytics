10b0c152a3239c939c3aa661f7ea01a9d8423f85
Daniel Chu <danchu123@gmail.com>
2017-01-09 11:47:14 -0600

    Changed device type to model, removed lat,lon,time

---
+++ db/migrate/20170109174040_rename_device_type_from_user_and_remove_lat_lon_time_from_visit.rb
@@ -0,0 +1,8 @@
+class RenameDeviceTypeFromUserAndRemoveLatLonTimeFromVisit < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :users, :device_type, :device_model
+    remove_column :visits, :latitude, :float
+    remove_column :visits, :longitude, :float
+    remove_column :visits, :time, :datetime
+  end
+end
