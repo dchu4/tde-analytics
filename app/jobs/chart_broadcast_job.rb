c5cf109606c1c92c898c5f984de557df07b97755
Daniel Chu <danchu123@gmail.com>
2017-01-14 00:02:10 -0600

    more redis

--- app/jobs/chart_broadcast_job.rb
+++ app/jobs/chart_broadcast_job.rb
@@ -1,7 +1,7 @@
 class ChartBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform ()
+  def perform
     # Do something later
     ActionCable.server.broadcast "chart_channel", {}
   end
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,5 @@
 class Visit < ApplicationRecord
-  after_create_commit { ChartBroadcastJob.perform }
+  after_create_commit { ChartBroadcastJob.perform_last }
 
   belongs_to :user
   belongs_to :product
