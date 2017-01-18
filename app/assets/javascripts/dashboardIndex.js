381f62fcd578e18965e120b3f0ee24e1dedef177
Amy Chan <chamy09@gmail.com>
2017-01-18 13:15:22 -0600

    changed color scheme

--- app/assets/javascripts/dashboardIndex.js
+++ app/assets/javascripts/dashboardIndex.js
@@ -5,10 +5,10 @@ var citiesChart;
 var deviceOsChart;
 var deviceModelChart;
 
-Chart.defaults.global.defaultColor = 'rgb(255, 99, 132)';
+Chart.defaults.global.defaultColor = '#F05A28';
 Chart.defaults.global.elements.responsive = true;
 
-var colorArray = ['rgb(255, 99, 132)', '#4BB5CC', '#ffd777','rgb(255, 99, 132)', '#4BB5CC', '#ffd777', 'rgb(255, 99, 132)', '#4BB5CC', '#ffd777']
+var colorArray = ['#FF9999', '#EE4036', '#E3F14F', '#F05A28', '#186CBB', '#A11C14', '#1D61A1']
 
 $(document).ready(function() {
 
@@ -19,8 +19,8 @@ $(document).ready(function() {
       datasets: [{
         label: 'Number of Visits',
         data: result["numbers"],
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: '#FF9999',
+        borderColor: '#FF9999',
         borderWidth: 2,
         fill: false
       }]
